d80d353360277dfb1a2480798dbdef75985237a6
Alex Kagan <alexkagan985@gmail.com>
2023-01-16 15:58:09 +0100

    small changes

--- src/components/AlexK.js
+++ src/components/AlexK.js
@@ -1,6 +1,7 @@
 import React from "react";
 import alexK from "./alexK.css";
 import coffeeGuy from "./photoAlex/coffeeGuy.png";
+import background from "./assets/coffee_bg.png"
 
 export default function AlexK() {
   return (
@@ -11,7 +12,8 @@ export default function AlexK() {
             Take away<br></br> sustainable
           </h2>
         </div>
-        <div id="middle">
+        <div id="middle" div style={{
+      backgroundImage: url(${background})}}>
           <h3 id="middleText">
             Bring your cup<br></br> and pay 0,30 less
           </h3>
