f113d1d60bfb89920719e05da196d51d185e88d5
Alex <alx.stanculescu@gmail.com>
2023-01-16 10:51:22 +0100

    adds scafold

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Sian />
+      <Radu />
+      <AlexK />
+      <Lukas />
+      <Alex />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
